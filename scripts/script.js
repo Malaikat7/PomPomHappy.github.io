a1b9a7ecd0649e11b13c3f508172f8f025eaf828
Malaikat7 <145450568+Malaikat7@users.noreply.github.com>
2024-06-01 17:00:55 +0200

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+const neutralpompom = document.querySelector('#pompom-image');
+const hereforyouButton = document.querySelector('#btn1');
+const lovelyButton = document.querySelector('#btn2');
+const yourecuteButton = document.querySelector('#btn3');
+const pompomImage = document.querySelector('#pompom-image');
+
+// Array images, hulp van chatGPT
+const pompomhappinessgrowing = [
+    'images/pompom1.jpg', // 10%
+    'images/pompom2.jpg', // 25%
+    'images/pompom3.jpg', // 50%
+    'images/pompom4.jpg', // 75%
+    'images/pompom5.jpg'  // 100%
+];
+
+// https://youtu.be/aTiOiMuSznE?si=9e8gw5b65YNpukYa, audio code van chatGPT
+const clickSound = new Audio('audio/twinkleaudio.mpeg');
+
+// begin happiness level
+let happiness = 10;
+let happinessindex = 0;
+
+//met hulp van student
+function growinghappiness(){
+    if (happinessindex < pompomhappinessgrowing.length){
+        neutralpompom.src = pompomhappinessgrowing[happinessindex];
+// audio code van chatGPT
+        clickSound.play();
+        happinessindex = happinessindex + 1;
+    } else {
+        alert("PomPom is fully happy, thank you!"); // hulp van chatGPT
+    }
+}
+
+hereforyouButton.addEventListener('click', growinghappiness);
+lovelyButton.addEventListener('click', growinghappiness);
+yourecuteButton.addEventListener('click', growinghappiness);
+
