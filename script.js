13c40ab1dc0eac63b725fba94bf053412de10aed
Malaikat7 <145450568+Malaikat7@users.noreply.github.com>
2024-06-04 00:51:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-const neutralpompom = document.querySelector('#pompom-image');
+const sadpompom = document.querySelector('#pompom-image');
 const hereforyouButton = document.querySelector('#btn1');
 const lovelyButton = document.querySelector('#btn2');
 const yourecuteButton = document.querySelector('#btn3');
-const pompomImage = document.querySelector('#pompom-image');
 
-// Array images, hulp van chatGPT
+//https://nl.pinterest.com/ciipiiyyy_/sanrio-pompompurin/, hulp van chatGPT
 const pompomhappinessgrowing = [
-    'Images/pompom1.jpg', // 10%
-    'Images/pompom2.jpg', // 25%
-    'Images/pompom3.jpg', // 50%
-    'Images/pompom4.jpg', // 75%
-    'Images/pompom5.jpg'  // 100%
+    'Images/pompom1.jpg', 
+    'Images/pompom2.jpg', 
+    'Images/pompom3.jpg', 
+    'Images/pompom4.jpg', 
+    'Images/pompom5.jpg'  
 ];
 
-// https://youtu.be/aTiOiMuSznE?si=9e8gw5b65YNpukYa, audio code van chatGPT
+// https://youtu.be/aTiOiMuSznE?si=9e8gw5b65YNpukYa, code van chatGPT
 const clickSound = new Audio('Audio/twinkleaudio.mpeg');
 
-// begin happiness level
-let happiness = 10;
 let happinessindex = 0;
 
 //met hulp van student
 function growinghappiness(){
     if (happinessindex < pompomhappinessgrowing.length){
-        neutralpompom.src = pompomhappinessgrowing[happinessindex];
-// audio code van chatGPT
-        clickSound.play();
+        sadpompom.src = pompomhappinessgrowing[happinessindex];
+        clickSound.play(); // audio code van chatGPT
         happinessindex = happinessindex + 1;
     } else {
         alert("PomPom is fully happy, thank you!"); // hulp van chatGPT
